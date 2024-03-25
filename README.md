424a32ea01657395e2b87ca885c333e2c4a79581
Juan Cuevas <juanandrescuevas14@gmail.com>
2024-03-24 22:43:30 -0400

    Actualización del README y los requisitos, y agregado de código para generar una tabla de colores.

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# school-schedule
+# School Schedule
+
+Este proyecto tiene como objetivo resolver el problema de asignar materias a profesores de una escuela. Para ello, se utiliza el lenguaje de programación Python, en donde usando la librería `sage` se puede resolver el problema de asignación de materias a profesores de manera óptima.
+
+## Uso
+
+Primeramente, es necesario instalar el paquete `sage` en su versión 9.5. Para ello, se puede seguir el siguiente [enlace](https://doc.sagemath.org/html/en/installation/). O usar el siguiente comando si usa alguna distribución de Linux o WSL basada en Debian:
+
+```bash
+sudo apt install sagemath
+```
+
+Posteriormente, es necesario tener las librerias de python necesarias. Para ello, se puede usar el siguiente comando:
+
+```bash
+pip install -r requirements.txt # o pip3 si es necesario
+```
+
+Finalmente, se puede ejecutar el programa con el siguiente comando:
+
+```bash
+./runner.sh <archivo_de_entrada>
+```
+
+Donde `<archivo_de_entrada>` es el archivo que contiene la información de las materias y los profesores. Un ejemplo de este archivo es el siguiente:
+
+<!-- TODO: Poner ejemplo -->
+```json
+{
+    "colegio": "Colegio 1",
+    "profesores": [
+    {
+      "nombre": "Pedro",
+      "materias": ["Matemáticas", "Física"],
+      "disponibilidad": {
+        "lunes": ["8:00", "10:00"],
+        "martes": ["10:00", "12:00"]
+      }
+    },
+    {
+      "nombre": "Maria",
+      "materias": ["Historia", "Literatura"],
+      "disponibilidad": {
+        "lunes": ["8:00", "10:00"],
+        "martes": ["8:00", "10:00"]
+      }
+    }
+  ],
+  "aulas": ["Aula 1", "Aula 2", "Aula 3"],
+  "hora_inicio": "07:00:00.000",
+  "hora_final": "19:00:00.000"
+}
+```
+<!-- TODO: Confirmar si es .pdf o .txt -->
+Finalmente, el programa generará un archivo de salida con la información de las materias asignadas a los profesores. Este archivo se llamará `oferta_<colegio>.pdf`
+
+> [!NOTE]
+> Es opcional instalar las librerias de Python, el script `runner.sh` se encarga de instalarlas si no están instaladas.
---
+++ runner.sh
@@ -0,0 +1,94 @@
+# !/bin/bash
+
+# Script que ejecuta la conversión de JSON a DIMACS y la resolución de SAT.
+# Devuelve el tiempo total de ejecución y si el problema es satisfacible o no.
+# Ademas, retorna un archivo .pdf con la solución del problema.
+
+# Uso: ./runner.sh <archivo JSON>
+# Ejemplo: ./runner.sh test.json
+
+# Verifica que la cantidad de argumentos sea la correcta. (Mayor o igual a 1) $# < 1
+if [ $# -lt 1 ]; then
+    echo -e "\033[91;1mError:\033[0m Cantidad de argumentos incorrecta."
+    echo -e "\033[93;1mUso:\033[0m ./runner.sh <archivo JSON> <archivo JSON> ... <archivo JSON>"
+    exit 1
+fi
+
+# Si el parametro es -h o --help, se imprime la ayuda.
+if [ $1 == "-h" ] || [ $1 == "--help" ]; then
+    echo -e "\033[93;1mUso:\033[0m ./runner.sh <archivo JSON> <archivo JSON> ... <archivo JSON>"
+    echo -e "\033[93;1mEjemplo:\033[0m ./runner.sh test.json"
+    exit 0
+fi
+
+FILES=$@
+
+# Verifica que cada archivo exista.
+for FILE in $FILES; do
+    if [ ! -f $FILE ]; then
+        echo -e "\033[91;1mError:\033[0m El archivo $FILE no existe."
+        exit 1
+    fi
+done
+
+# Verifica que los archivos sean de tipo JSON.
+for FILE in $FILES; do
+    if [[ $FILE != *.json ]]; then
+        echo -e "\033[91;1mError:\033[0m El archivo $FILE no es de tipo JSON."
+        exit 1
+    fi
+done
+
+FILE_TIME="times.txt"
+# Verificamos si el archivo de tiempos no existe, si no existe, lo creamos.
+if [ ! -f $FILE_TIME ]; then
+    echo -e "Archivo\t Convertir a DIMCAS\t Resolver SAT\tTiempo Total\tSatisfacible" > $FILE_TIME
+fi
+
+# Verificamos si las librerias de requirements.txt estan instaladas
+printf "\033[93;1mVerificando librerías...\033[0m\n"
+# Declaramos un contador para la barra de progreso
+N=0
+LIBS=$(wc -l < requirements.txt)
+while read -r line; do
+    # Obtenemos el nombre de la librería. El formato es: <nombre>==<versión>
+    PACKAGE=$(echo $line | cut -d'=' -f1)
+    if ! pip3 -q show $PACKAGE > /dev/null 2>&1; then
+        printf "\033[93;1mInstalando:\033[0m $PACKAGE\n"
+        pip3 -q install $line > /dev/null 2>&1
+    fi
+    N=$((N+1))
+    # Barra de progreso
+    # La barra de progreso tiene el formato: [=====    ] 50%
+    printf "["
+    for ((i=0; i < $((N*10/LIBS)); i++)); do printf "\033[92;1m=\033[0m"; done
+    for ((i=$N; i < $((LIBS/10)); i++)); do printf " "; done
+    printf "] %d%%\r" $((N*100/LIBS))
+done < requirements.txt
+echo -e "\n"
+
+# Ejecutar la conversión de JSON a PDF y la resolución de SAT
+echo -e "\033[93;1mConvirtiendo JSON a ICS...\033[0m"
+# Si solo es un archivo, no se imprime el nombre del archivo
+if [ $# -eq 1 ]; then
+    printf "\033[93;1mConvirtiendo:\033[0m $FILES\n"
+    python3 main.py $FILES
+else
+    N_FILES=$#
+    N=0
+    for FILE in $FILES; do
+        printf "["
+        for ((i=0; i < $((N*10/N_FILES)); i++)); do printf "\033[92;1m=\033[0m"; done
+        printf "\033[92;1m>\033[0m"
+        printf "] %d%%\r" $((N*100/N_FILES))
+        python3 main.py $FILE
+        # Eliminamos el archivo .cnf
+	    rm -f $(echo $FILE | sed 's/.json/.cnf/')
+        N=$((N+1))
+    done
+    printf "["
+    for ((i=0; i < $((N*10/N_FILES)); i++)); do printf "\033[92;1m=\033[0m"; done
+    printf "] %d%%\r" $((N*100/N_FILES))
+    echo -e "\n"
+fi
+echo -e "El tiempo de ejecución de cada archivo se encuentra en el archivo \033[93;1m$FILE_TIME\033[0m."
