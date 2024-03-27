cf0096aa02c8983d632cf3a0e2ac21ded79b10f4
Ana <73360276+anna-ayn@users.noreply.github.com>
2024-03-27 13:30:49 -0400

    fixed

--- modules/cnf_maker.py
+++ modules/cnf_maker.py
@@ -29,7 +29,7 @@ def A_table(p:int, subjects:int, classrooms:int, hours:int):
 # restriccion 0 a CNF
 # Un profesor no esta disponible para dar clases en un dia d y hora h si no esta en su disponibilidad
 # y tampoco puede dar una materia si no esta en su disponibilidad
-def c0(problem, A: List[List[List[List[List[str]]]]], disp_teachers, start_time: str, end_time: str, n_teachers: int, n_subjects: int, n_classrooms:int, n_hours:int) -> None:
+def c0(problem, A: List[List[List[List[List[str]]]]], disp_teachers, n_teachers: int, n_subjects: int, n_classrooms:int, n_hours:int) -> None:
     for p in range(n_teachers):
         for m in range(n_subjects):
             if m not in disp_teachers[p]["materias"]:
@@ -46,109 +46,88 @@ def c0(problem, A: List[List[List[List[List[str]]]]], disp_teachers, start_time:
                         else:
                             if h not in disp_teachers[p]["disponibilidad"][d] or h==disp_teachers[p]["disponibilidad"][d][-1]:
                                 for a in range(n_classrooms):
-                                    problem.add_constr(Not(Bool(A[p][m][a][i][h])))
+                                    problem.add_constr(Not(Bool(A[p][m][a][i][h])))   
 
-# restriccion 1 a CNF
-# Un profesor solo puede impartir una materia a la vez.
-def c1(problem, A: List[List[List[List[List[str]]]]], n_teachers: int, n_subjects: int, n_classrooms: int, n_hours:int) -> None:
-    for m1 in range(n_subjects):
-        for m2 in range(n_subjects):
-            if m1 == m2:
-                continue
-            for p in range(n_teachers):
-                for a1 in range(n_classrooms):
-                    for a2 in range(n_classrooms):
-                        for d in range(5):
-                            for h in range(n_hours):
-                                problem.add_constr(Or(Not(Bool(A[p][m1][a1][d][h])), Not(Bool(A[p][m2][a2][d][h]))))
-    for a1 in range(n_classrooms):
-        for a2 in range(n_classrooms):
-            if a1 == a2:
-                continue
-            for p in range(n_teachers):
-                for m1 in range(n_subjects):
-                    for m2 in range(n_subjects):
-                        for d in range(5):
-                            for h in range(n_hours):
-                                problem.add_constr(Or(Not(Bool(A[p][m1][a1][d][h])), Not(Bool(A[p][m2][a2][d][h]))))
-
-
-# restriccion 2 a CNF
-# En una aula sólo puede impartir una materia a la vez.
-def c2(problem, A: List[List[List[List[List[str]]]]], n_teachers: int, n_subjects: int, n_classrooms: int, n_hours:int) -> None:
-    for p1 in range(n_teachers):
-        for p2 in range(n_teachers):
-            if p1 == p2:
-                continue
-            for m1 in range(n_subjects):
-                for m2 in range(n_subjects):
-                    for a in range(n_classrooms):
-                        for d in range(5):
-                            for h in range(n_hours):
-                                problem.add_constr(Or(Not(Bool(A[p1][m1][a][d][h])), Not(Bool(A[p2][m2][a][d][h]))))
-    for m1 in range(n_subjects):
-        for m2 in range(n_subjects):
-            if m1 == m2:
-                continue
-            for p1 in range(n_teachers):
-                for p2 in range(n_teachers): 
-                    for a in range(n_classrooms):
-                        for d in range(5):
-                            for h in range(n_hours):
-                                problem.add_constr(Or(Not(Bool(A[p1][m1][a][d][h])), Not(Bool(A[p2][m2][a][d][h]))))
-
-
-# restriccion 4 a CNF:
-# Cada clase dura exactamente dos horas
-def c3(problem, A: List[List[List[List[List[str]]]]], n_teachers: int, n_subjects: int, n_classrooms: int, n_hours:int) -> None:
-    for p in range(n_teachers):
-        for m in range(n_subjects):
-            for a in range(n_classrooms):
-                for d in range(5):
-                    for h1 in range(n_hours):
-                        expr = Not(Bool(A[p][m][a][d][h1]))
-                        for h2 in range(n_hours):
-                            if ((h1 == h2 + 1 or h1 + 1 == h2) and (h1 != h2 + 1 or h1 + 1 != h2)):
-                                expr = expr | Bool(A[p][m][a][d][h2])
-                        problem.add_constr(expr)
-
-# restriccion 5 a CNF:
 # Cada materia se imparte dos veces por semana en el mismo salón designado.
-def c4(problem, A: List[List[List[List[List[str]]]]], n_teachers: int, n_subjects: int, n_classrooms: int, n_hours:int) -> None:
+def c1(problem, A: List[List[List[List[List[str]]]]], disp_teachers, n_teachers: int, n_subjects: int, n_classrooms: int, n_hours:int) -> None:
     for p in range(n_teachers):
         for m in range(n_subjects):
+            if m not in disp_teachers[p]["materias"]:
+                continue
             Ors = []
             for a in range(n_classrooms):
-                if a == 0:
-                    continue
-                for d1 in range(5):
-                    if d1 == 4:
+                for i1, d1 in enumerate(["lunes", "martes", "miercoles", "jueves", "viernes"]):
+                    if d1 not in disp_teachers[p]["disponibilidad"].keys() or d1 == "viernes":
                         continue
                     for h1 in range(n_hours):
-                        for d2 in range(5):
-                            if d2 <= d1:
+                        if h1 not in disp_teachers[p]["disponibilidad"][d1] or h1==disp_teachers[p]["disponibilidad"][d1][-1]:
+                            continue
+                        for i2, d2 in enumerate(["lunes", "martes", "miercoles", "jueves", "viernes"]):
+                            if d2 not in disp_teachers[p]["disponibilidad"].keys() or d2 <= d1:
                                 continue
                             for h2 in range(n_hours):
-                                if h1 == h2:
+                                if h2 not in disp_teachers[p]["disponibilidad"][d2] or h2==disp_teachers[p]["disponibilidad"][d2][-1]:
                                     continue
-                                expr = Bool(A[p][m][a][d1][h1]) & Bool(A[p][m][a][d2][h2])
+                                expr = Bool(A[p][m][a][i1][h1]) & Bool(A[p][m][a][i2][h2])
                                 Ors.append(expr)
-            problem.add_constr(Or(Ors))
+            if len(Ors) > 0:
+                problem.add_constr(Or(Ors))
+                                 
 
-# restriccion 6 a CNF:
-# Un profesor no puede impartir una misma materia más de una vez en un mismo dia.
-def c5(problem, A: List[List[List[List[List[str]]]]], n_teachers: int, n_subjects: int, n_classrooms: int, n_hours:int) -> None:
-    for p in range(n_teachers):
-        for m in range(n_subjects):
+# restriccion 1 a CNF
+# Dos clases no pueden ocurrir al mismo tiempo para un salon y donde cada clase dura exactamente dos horas.
+def c2(problem, A: List[List[List[List[List[str]]]]], disp_teachers, n_teachers: int, n_subjects: int, n_classrooms: int, n_hours:int) -> None:
+    for p1 in range(n_teachers):
+        for m1 in range(n_subjects):
+            if m1 not in disp_teachers[p1]["materias"]:
+                continue
             for a in range(n_classrooms):
-                for d in range(5):
+                for i, d in enumerate(["lunes", "martes", "miercoles", "jueves", "viernes"]):
+                    if d not in disp_teachers[p1]["disponibilidad"].keys():
+                        continue
                     for h1 in range(n_hours):
-                        expr = Bool(A[p][m][a][d][h1])
-                        for h2 in range(n_hours):
-                            if h2 == h1 and h2 == h1+1:
+                        if h1 not in disp_teachers[p1]["disponibilidad"][d] or h1==disp_teachers[p1]["disponibilidad"][d][-1]:
+                            continue
+                        for p2 in range(n_teachers):
+                            if d not in disp_teachers[p2]["disponibilidad"].keys():
                                 continue
-                            problem.add_constr(expr | Not(Bool(A[p][m][a][d][h2])))
-
+                            for m2 in range(n_subjects):
+                                if m2 not in disp_teachers[p2]["materias"]:
+                                    continue
+                                if p1 == p2 and m1 == m2:
+                                    continue
+                                for h2 in range(n_hours):
+                                    if h2 not in disp_teachers[p2]["disponibilidad"][d] or h2==disp_teachers[p2]["disponibilidad"][d][-1]:
+                                        continue
+                                    if h2 != h1 and h2 != h1 + 1:
+                                        continue 
+                                    expr = Or(Not(Bool(A[p1][m1][a][i][h1])), Not(Bool(A[p2][m2][a][i][h2])))
+                                    problem.add_constr(expr)
+# restriccion 3 a CNF:
+# Un profesor solo puede impartir una materia a la vez
+def c3(problem, A: List[List[List[List[List[str]]]]], disp_teachers, n_teachers: int, n_subjects: int, n_classrooms: int, n_hours:int) -> None:
+    for p in range(n_teachers):
+        for m1 in range(n_subjects):
+            if m1 not in disp_teachers[p]["materias"]:
+                continue
+            for a1 in range(n_classrooms):
+                for i, d in enumerate(["lunes", "martes", "miercoles", "jueves", "viernes"]):
+                    if d not in disp_teachers[p]["disponibilidad"].keys():
+                        continue
+                    for h in range(n_hours):
+                        if h not in disp_teachers[p]["disponibilidad"][d] or h==disp_teachers[p]["disponibilidad"][d][-1]:
+                            continue
+                        for m2 in range(n_subjects):
+                            if m2 not in disp_teachers[p]["materias"]:
+                                continue
+                            for a2 in range(n_classrooms):
+                                if m1 == m2 and a1 == a2:
+                                    continue
+                                expr = Or(Not(Bool(A[p][m1][a1][i][h])), Not(Bool(A[p][m2][a2][i][h])))
+                                problem.add_constr(expr)
+                                if h + 1 != n_hours:
+                                    expr = Or(Not(Bool(A[p][m1][a1][i][h])), Not(Bool(A[p][m2][a2][i][h + 1])))
+                                    problem.add_constr(expr)
 
 # traducir restricciones a formato dimacs
 def todimacs(start_time: str, end_time: str, p: int, subjects: int, classrooms: int, hours: int, disp_teachers: list, filename: str) -> str:
@@ -166,23 +145,17 @@ def todimacs(start_time: str, end_time: str, p: int, subjects: int, classrooms: 
     threads = []
 
     # repartir el trabajo de modelar las restricciones entre los hilos
-    t0 = threading.Thread(target=c0, args=(problem, A, disp_teachers, start_time, end_time, p, subjects, classrooms,hours))
+    t0 = threading.Thread(target=c0, args=(problem, A, disp_teachers, p, subjects, classrooms,hours))
     threads.append(t0)
 
-    t1 = threading.Thread(target=c1, args=(problem, A, p, subjects, classrooms, hours))
+    t1 = threading.Thread(target=c1, args=(problem, A, disp_teachers, p, subjects, classrooms, hours))
     threads.append(t1)
 
-    #t2 = threading.Thread(target=c2, args=(problem, A, p, subjects, classrooms, hours))
-    #threads.append(t2)
+    t2 = threading.Thread(target=c2, args=(problem, A, disp_teachers, p, subjects, classrooms, hours))
+    threads.append(t2)
 
-    #t3 = threading.Thread(target=c3, args=(problem, A, p, subjects, classrooms, hours))
-    #threads.append(t3)
-
-    #t4 = threading.Thread(target=c4, args=(problem, A, p, subjects, classrooms, hours))
-    #threads.append(t4)
-
-    #t5 = threading.Thread(target=c5, args=(problem, A, p, subjects, classrooms, hours))
-    #threads.append(t5)
+    t3 = threading.Thread(target=c3, args=(problem, A, disp_teachers, p, subjects, classrooms, hours))
+    threads.append(t3)
 
     # iniciar los hilos
     for thread in threads:
@@ -191,7 +164,8 @@ def todimacs(start_time: str, end_time: str, p: int, subjects: int, classrooms: 
     # esperar a que los hilos terminen
     for thread in threads:
         thread.join()
-    
+
+
     # convertir el problema a formato DIMACS CNF
     cnf = problem.to_cnf_dimacs()
     with open(outputCointraints, 'w') as f:
@@ -202,22 +176,25 @@ def todimacs(start_time: str, end_time: str, p: int, subjects: int, classrooms: 
     s.add_clauses(cnf.clauses)
     s.solve()
     interpretation = cnf.decode_dimacs(s.model())
+    if len(interpretation) > 0:
+        model = []
 
-    model = []
-    for e in interpretation:
-        if isinstance(e, Not):
-            continue
-        model.append(str(e))
-    
-    
-    for p in range(p):
-        for m in range(subjects):
-            for a in range(classrooms):
-                for d in range(5):
-                    for h in range(hours):
-                        if '~' + A[p][m][a][d][h] in model:
-                            continue
-                        print(f"Variable is true for teacher {p}, subject {m}, classroom {a}, day {d}, and hour {h}")
+        for e in interpretation:
+            model.append(str(e))
+        
+        i = 0
+        for e in model:
+            if e[0] == "~":
+                i = i + 1
 
-
+        for p in range(p):
+            for m in range(subjects):
+                for a in range(classrooms):
+                    for d in range(5):
+                        for h in range(hours):
+                            if "~" + A[p][m][a][d][h] in model:
+                                continue
+                            print(f"Variable is true for teacher {p}, subject {m}, classroom {a}, day {d}, and hour {h}")
+    else:
+        print("No model found")
     return (outputCointraints, cnf)
